9e6b4db8ebd1654229560c38bff58c4473ea112d
ccocchi <cocchi.c@gmail.com>
2012-07-25 19:22:10 +0200

    Make render work with same signature as standard RABL gem

--- lib/rabl-rails/renderer.rb
+++ lib/rabl-rails/renderer.rb
@@ -1,24 +1,38 @@
 require 'rabl-rails/renderers/base'
 require 'rabl-rails/renderers/json'
 
-module Renderer
-  mattr_reader :view_path
-  @@view_path = 'app/views'
+module RablRails
+  module Renderer
+    mattr_reader :view_path
+    @@view_path = 'app/views'
   
-  def render(object, template, options = {})
-    format = options.delete(:format) || 'json'
-    source = find_template(template, format, options.delete(:view_path))
-    compiled_template = Compiler.new.compile_source(source)
+    class Context
+      def initialize
+        @_assigns = {}
+      end
+
+      def assigns
+        @_assigns
+      end
+    end
+  
+    def render(object, template, options = {})
+      format = options.delete(:format) || 'json'
+      source = find_template(template, format, options.delete(:view_path))
+      compiled_template = Compiler.new.compile_source(source)
     
-    # TODO: context needs to be set from options
-    Renderers.const_get(format.upcase!).new(context).render(compiled_template)
-  end
+      c = Context.new
+      c.assigns[compiled_template.data.to_s[1..-1]] = object if compiled_template.data
+
+      Renderers.const_get(format.upcase!).new(c).render(compiled_template)
+    end
   
-  private
+    private
   
-  def find_template(name, format, view_path = nil)
-    view_path ||= self.view_path
-    path = File.join(view_path, "#{name}.#{format}.rabl")
-    File.exists?(path) : File.read(path) : nil
+    def find_template(name, format, view_path = nil)
+      view_path ||= self.view_path
+      path = File.join(view_path, "#{name}.#{format}.rabl")
+      File.exists?(path) ? File.read(path) : nil
+    end
   end
 end
