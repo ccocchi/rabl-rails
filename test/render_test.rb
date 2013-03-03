b4877711d5775276bcfac90cf2d438f84f2c1c06
ccocchi <cocchi.c@gmail.com>
2013-03-03 13:27:00 +0100

    Split tests

--- test/render_test.rb
+++ test/render_test.rb
@@ -60,14 +60,25 @@ class RenderTest < ActiveSupport::TestCase
     assert_equal %q({"user":{"extended_name":"Marty"}}), RablRails.render(@user, 'extend', view_path: @tmp_path)
   end
 
-  test "format can be passed as symbol or string" do
+  test "format can be passed as symbol" do
     File.open(@tmp_path + "show.json.rabl", "w") do |f|
       f.puts %q{
         object :@user
         attributes :id, :name
       }
     end
+
     assert_equal %q({"user":{"id":1,"name":"Marty"}}), RablRails.render(@user, 'show', view_path: @tmp_path, format: :json)
+  end
+
+  test "format can be passed as upcased string" do
+    File.open(@tmp_path + "show.json.rabl", "w") do |f|
+      f.puts %q{
+        object :@user
+        attributes :id, :name
+      }
+    end
+
     assert_equal %q({"user":{"id":1,"name":"Marty"}}), RablRails.render(@user, 'show', view_path: @tmp_path, format: 'JSON')
   end
 
