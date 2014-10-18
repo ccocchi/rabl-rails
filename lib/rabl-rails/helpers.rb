b836dd8a864888862d3f3a49003917c523a33a21
Sergey Besedin <kr3ssh@gmail.com>
2014-10-19 01:54:06 +0400

    more accurate collection determination

--- lib/rabl-rails/helpers.rb
+++ lib/rabl-rails/helpers.rb
@@ -3,7 +3,7 @@ module RablRails
     def collection?(resource)
       klass = resource.class
 
-      resource && resource.respond_to?(:each) &&
+      resource && resource.respond_to?(:to_ary) &&
         RablRails.configuration.non_collection_classes.none? { |k| klass <= k }
     end
   end
