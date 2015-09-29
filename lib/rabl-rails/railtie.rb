780a730234f13f3503d3846f864b5809e9db17ad
itkin <nicolas.papon@webflows.fr>
2015-09-29 02:50:39 +0200

    make sure action controller responder is set after initializers are loaded

--- lib/rabl-rails/railtie.rb
+++ lib/rabl-rails/railtie.rb
@@ -1,12 +1,10 @@
 module RablRails
   class Railtie < Rails::Railtie
-    initializer "rabl.initialize" do |app|
-      ActiveSupport.on_load(:action_view) do
-        ActionView::Template.register_template_handler :rabl, RablRails::Handlers::Rabl
-      end
-      ActiveSupport.on_load :action_controller do
-        self.responder = RablRails::Responder if RablRails.configuration.use_custom_responder
-      end
+    initializer 'rabl.actionview' do
+      ActionView::Template.register_template_handler :rabl, RablRails::Handlers::Rabl
+    end
+    config.after_initialize do
+      ActionController::Base.responder = RablRails::Responder if RablRails.configuration.use_custom_responder
     end
   end
 end
