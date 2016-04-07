a7bd42b47da8dea99b4bbf1f15d99ae4de099072
= <kuykendall.ph@gmail.com>
2016-04-07 17:32:50 -0400

    Tweaking collection check to check for Enumerable in ancestry.

--- lib/rabl-rails/helpers.rb
+++ lib/rabl-rails/helpers.rb
@@ -3,7 +3,7 @@ module RablRails
     def collection?(resource)
       klass = resource.class
 
-      resource && resource.respond_to?(:each) &&
+      resource && resource.respond_to?(:each) && resource.kind_of?(Enumerable) &&
         klass.ancestors.none? { |a| RablRails.configuration.non_collection_classes.include? a.name }
     end
   end
