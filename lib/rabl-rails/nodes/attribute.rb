3e36043e6de576780becdf99b34208fe76dc1ace
ccocchi <cocchi.c@gmail.com>
2016-04-09 19:36:10 +0200

    Remove Forwardable in favor of code (making `each` 9% faster yay)

--- lib/rabl-rails/nodes/attribute.rb
+++ lib/rabl-rails/nodes/attribute.rb
@@ -1,16 +1,19 @@
-require 'forwardable'
-
 module RablRails
   module Nodes
     class Attribute
-      extend Forwardable
-
-      def_delegators :@hash, :[]=, :each
       attr_reader   :hash
       attr_accessor :condition
 
       def initialize(hash = {})
         @hash = hash
+      end
+
+      def []=(key, value)
+        @hash[key] = value
+      end
+
+      def each(&block)
+        @hash.each(&block)
       end
     end
   end
