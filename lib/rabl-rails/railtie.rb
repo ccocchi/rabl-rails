f5bc0da89b019a2879824509ebea9bbf04363e34
Alex Beznos <beznosa@yahoo.com>
2016-10-04 22:43:48 +0300

    add necessary modules for rails 5 rabl rendering

--- lib/rabl-rails/railtie.rb
+++ lib/rabl-rails/railtie.rb
@@ -6,6 +6,21 @@ module RablRails
       end
     end
 
+    if Rails::VERSION::MAJOR >= 5
+      module ::ActionController
+        module ApiRendering
+          include ActionView::Rendering
+        end
+      end
+
+      ActiveSupport.on_load :action_controller do
+        if self == ActionController::API
+          include ActionController::Helpers
+          include ActionController::ImplicitRender
+        end
+      end
+    end
+
     config.after_initialize do
       ActionController::Base.responder = RablRails::Responder if RablRails.configuration.use_custom_responder
     end
