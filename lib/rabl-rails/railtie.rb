41d83466357a20f374ee44eeeb2beca55479f47b
christopher <cocchi.c@gmail.com>
2017-08-02 15:06:51 +0200

    Compatibility with `ActionController::API`

--- lib/rabl-rails/railtie.rb
+++ lib/rabl-rails/railtie.rb
@@ -4,6 +4,21 @@ module RablRails
       ActiveSupport.on_load(:action_view) do
         ActionView::Template.register_template_handler :rabl, RablRails::Handlers::Rabl
       end
+
+      if Rails::VERSION::MAJOR >= 5
+        module ::ActionController
+          module ApiRendering
+            include ActionView::Rendering
+          end
+        end
+
+        ActiveSupport.on_load :action_controller do
+          if self == ActionController::API
+            include ActionController::Helpers
+            include ActionController::ImplicitRender
+          end
+        end
+      end
     end
   end
 end
