b6feed8ad4b338ce4d2dd1abd5b5dc32aa825ba4
Marcello Barnaba <vjt@openssl.it>
2020-07-02 19:53:13 +0200

    add source parameter for rails 6

--- lib/rabl-rails/handler.rb
+++ lib/rabl-rails/handler.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/class/attribute'
 module RablRails
   module Handlers
     class Rabl
-      def self.call(template)
+      def self.call(template, source = nil)
         %{
           RablRails::Library.instance.
             get_rendered_template(#{template.source.inspect}, self, local_assigns)
